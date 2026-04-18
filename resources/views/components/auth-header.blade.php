460c8e5e6b1c87c958912ca333ab05d555adecb9
blindmelon07 <bryandetera07@gmail.com>
2026-04-18 12:08:21 +0800

    login

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -3,7 +3,8 @@
     'description',
 ])
 
-<div class="flex w-full flex-col text-center">
+<div class="flex w-full flex-col items-center text-center">
+    <img src="{{ asset('images/gghi logo (1).png') }}" alt="GGHI Logo" class="mb-4 h-20 w-auto" />
     <flux:heading size="xl">{{ $title }}</flux:heading>
     <flux:subheading>{{ $description }}</flux:subheading>
 </div>
